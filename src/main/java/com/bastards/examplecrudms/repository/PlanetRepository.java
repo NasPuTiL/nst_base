aef30c10a08cfd14dc956587232e2cf358c53789
Morteus <morteuss@protonmail.com>
2023-08-10 21:03:03 +0200

    Revert "Initial commit"
    This reverts commit ff9372647fbe0bd5e269dad32fde660e14aa8baa.

--- src/main/java/com/bastards/examplecrudms/repository/PlanetRepository.java
+++
@@ -1,7 +0,0 @@
-package com.bastards.examplecrudms.repository;
-
-import com.bastards.examplecrudms.model.Planet;
-import org.springframework.data.repository.ListCrudRepository;
-
-public interface PlanetRepository extends ListCrudRepository<Planet, Long>  {
-}
